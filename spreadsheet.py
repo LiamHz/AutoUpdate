da8e96fe98350a986ffe210af69cfb48ed11723c
Liam Hinzman <liam.hinzman@gmail.com>
2019-04-10 07:41:54 -0400

    Added minimum length to updates

--- spreadsheet.py
+++ spreadsheet.py
@@ -6,13 +6,21 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import random
 
+
+
+RANGE_NAME = "'April'"
+WEEK = 1
+INNOVATE_INDEX = 3          # Innovate and activate have a different number of columns before the first update
+ACTIVATE_INDEX = 5
+MIN_UPDATE_LENGTH = 500     # Minimum amount of character
+
+
+
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 COHORT_SPREADSHEET_IDs = ['1jHFtzPV8dUBlOpoXgBwnoLqb1d27KHyUZKDTqBNpe_8', '19bQjw_RzInIgFoMdHylQqXi05bp5p59TEIvwPxzXf1o', '1y85fw3YI9JIzoQDya0WewAWyMqhRAKy_ARgMeIhLQAc', '1or4TT9jHSMbT7Ey6ve1Jeb_bVhj-GxxelGjK0lYZlAk']
-RANGE_NAME = "'March'"
-WEEK = 4
 
 updates = []
 
@@ -55,13 +63,13 @@ def get_updates():
         else:
             for row in values:
                 try:
-                    # Cohort 1 has extra column for "ambition"
+                    # Cohort 1 has extra columns
                     if cohort == 0:
                         # Top row has headers for each column, including "name"
-                        if row[0] != '' and row[WEEK+3] != '' and row[0] != 'Name':
-                            updates.append([row[0], row[WEEK+3]])
-                    elif row[0] != '' and row[WEEK+2] != '' and row[0] != 'Name':
-                            updates.append([row[0], row[WEEK+2]])
+                        if row[0] != '' and row[WEEK+ACTIVATE_INDEX] != '' and row[0] != 'Name' and len(row[WEEK+ACTIVATE_INDEX]) > 500:
+                            updates.append([row[0], row[WEEK+ACTIVATE_INDEX]])
+                    elif row[0] != '' and row[WEEK+INNOVATE_INDEX] != '' and row[0] != 'Name' and len(row[WEEK+INNOVATE_INDEX]) > 500:
+                            updates.append([row[0], row[WEEK+INNOVATE_INDEX]])
                 # Ignore cell if empty
                 except:
                     pass
